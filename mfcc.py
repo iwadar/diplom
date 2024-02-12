aa4b6686cc2ee73442f6f4a593036ad0e6e1667d
Dasha <dasha@localhost>
2024-02-12 23:41:59 +0300

    start attempt compare long audio (don't work)

--- mfcc.py
+++ mfcc.py
@@ -1,8 +1,12 @@
 import librosa
 import numpy as np
-from fastdtw import fastdtw
 from scipy.spatial.distance import euclidean
 from dtw import dtw
+
+_ALLOWABLE_ERROR = 20
+_STANDART_STEP = 1
+_VALID_MATCH = 0.5
+_INCREASE_STEP = 2
 
 class MFCC:
     def __init__(self, fileName) -> None:
@@ -45,9 +49,13 @@ class MFCC:
 
         self.listFrames = np.array(list(map(lambda frame: frame * window, self.listFrames)))
 
-
     def mfcc(self, n_mfcc = 13):
-        self.listFrames = np.array(list(map(lambda frame: librosa.feature.mfcc(y=frame, sr=self.sampleRate, n_mfcc=n_mfcc),  self.listFrames)))
+        # Беру 0 массив, потому что не понятно до сих пор как формируется shape результата
+        self.listFrames = np.array(list(map(lambda frame: librosa.feature.mfcc(y=frame, sr=self.sampleRate, n_mfcc=n_mfcc, hop_length=self.length).T[0],  self.listFrames)))
+        
+        # self.listFrames = np.array(list(map(lambda frame: librosa.feature.mfcc(y=frame, sr=self.sampleRate, n_mfcc=n_mfcc,  hop_length=self.length, n_fft=len(self.listFrames)).T[0],  self.listFrames)))
+        
+        # print(self.listFrames)
 
 
     def calculateMFCC(self):
@@ -71,47 +79,45 @@ class Compare:
         """
         примем пока что они плюс минус равны по размеру
         """
-        # x = np.array([2, 0, 1, 1, 2, 4, 2, 1, 2, 0]).reshape(-1, 1)
-        # y = np.array([1, 1, 2, 4, 2, 1, 2, 0]).reshape(-1, 1)
-
-        # x = np.array(userFrames)
-        x = []
-        # x = userFrames.reshape(-1)
-        # print(x)
-        # print(y)
-
-        # # print(userFrames)
-        # y = referenceFrames.reshape(-1)
-
-        for frame in userFrames:
-            # print(frame)
-            frame = frame.reshape(-1)
-            x.append(frame)
-            # print(frame)
-            # print(userFrames[0])
-            # break
-        y = []
-        for frame in referenceFrames:
-            frame = frame.reshape(-1)
-            y.append(frame)
-
-        # # print(userFrames)
-        # # y = referenceFrames.reshape(-1)
-        # manhattan_distance = lambda x, y: np.abs(x - y)
-
-        d, cost_matrix, acc_cost_matrix, path = dtw(x, y, dist=euclidean)
+       
+        d, cost_matrix, acc_cost_matrix, path = dtw(userFrames, referenceFrames, dist=euclidean)
 
         min = np.min(cost_matrix)
         max = np.max(cost_matrix)
 
         sum = 0
-        for i in range(len(path[0])):
-            sum += cost_matrix[path[0][i]][path[1][i]]
+        for i in range(len(path[0]) - 1):
+            sum += cost_matrix[int(path[0][i])][int(path[1][i])]
 
         sum /= len(path[0])
 
         # print(cost_matrix)
-        print(1 - (sum - min) / (max - min))
+        # print(1 - (sum - min) / (max - min))
+        return (1 - (sum - min) / (max - min))
+
+    def crossValidationLongAudio(self, referenceFrames, userFrames):
+
+        if abs(len(referenceFrames) - len(userFrames)) <= _ALLOWABLE_ERROR:
+            return self.crossValidation(referenceFrames=referenceFrames, userFrames=userFrames)
+        
+        i = 0
+        maxMatch = 0.0
+        index = 0
+        sizeReference = len(referenceFrames)
+        while i != len(userFrames):
+            endSlice = i + sizeReference
+            if endSlice >= len(userFrames):
+                endSlice = len(userFrames)
+            if (result := self.crossValidation(referenceFrames=referenceFrames, userFrames=userFrames[i:endSlice])) < _VALID_MATCH:
+                i += _INCREASE_STEP
+            elif result > maxMatch:
+                maxMatch = result
+                index = i
+                i += _STANDART_STEP
+            else:
+                i += _STANDART_STEP
+        # print({maxMatch}')
+        return maxMatch, index
 
 
 
@@ -121,10 +127,12 @@ if __name__=='__main__':
     mfccRef.calculateMFCC()
     compare = Compare()
 
-    for file in ['/home/dasha/python_diplom/ASR/wavs/training/in_db_sd_low_spl/two-2-0-m.wav', '/home/dasha/python_diplom/ASR/wavs/training/in_db_sd_low_spl/seven-7-0-m.wav', '/home/dasha/python_diplom/ASR/wavs/training/in_db_sd_low_spl/one-1-5-m.wav', '/home/dasha/python_diplom/ASR/wavs/training/in_db_sd_low_spl/five-5-5-m.wav']:
+    # for file in ['/home/dasha/python_diplom/ASR/wavs/training/in_db_sd_low_spl/two-2-0-m.wav', '/home/dasha/python_diplom/ASR/wavs/training/in_db_sd_low_spl/seven-7-0-m.wav', '/home/dasha/python_diplom/ASR/wavs/training/in_db_sd_low_spl/one-1-5-m.wav', '/home/dasha/python_diplom/ASR/wavs/training/in_db_sd_low_spl/five-5-5-m.wav']:
+    for file in ['/home/dasha/python_diplom/wav/one-five.wav', '/home/dasha/python_diplom/wav/no-five.wav']:
         mfcc1 = MFCC(file)
         print(file.split('/')[-1])
         mfcc1.calculateMFCC()
-        compare.crossValidation(mfccRef.listFrames, mfcc1.listFrames)
+        print(compare.crossValidationLongAudio(mfccRef.listFrames, mfcc1.listFrames))
         print('-'*15)
+        # break
 
