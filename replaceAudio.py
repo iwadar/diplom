942759a77b8b4ee6f8629adf5cc681df79c6d460
Dasha <dasha@localhost>
2024-03-13 00:09:00 +0300

    продолжаем мучить сегмантацию по входному аудио. Слова с видимым периодом тишины разделяет

--- replaceAudio.py
+++ replaceAudio.py
@@ -4,17 +4,22 @@ from pydub import AudioSegment
 import matplotlib.pyplot as plt
 import sys
 from math import fabs
+import noisereduce as nr
+
 
 _SECOND_TO_MILLISECOND = 1000
 
 
 class SearchWord:
     def __init__(self, filename) -> None:
-        _wordTime = 200 # время за которое говорят 1 слово - 200 мс
+        _wordTime = 2 # время за которое говорят 1 слово - 200 мс
         self.audioData = AudioSegment.from_file(filename)
         self.sampleRate = self.audioData.frame_rate
         self.maxAmplutideInAllSample = 0
-        self.initSizeWindow = int(_wordTime * self.sampleRate / _SECOND_TO_MILLISECOND)
+        self.sizeWindow = int(_wordTime * self.sampleRate / _SECOND_TO_MILLISECOND)
+        self.threshold_1 = 0
+        self.threshold_2 = 0
+
 
         #TODO посмотреть может быть загружать данный таким способом, а не librosa будет быстрее
         # print((np.array(self.audioData.get_array_of_samples(), dtype=np.float32).reshape((-1, self.audioData.channels)) / (
@@ -23,34 +28,68 @@ class SearchWord:
 
 
     def normalization(self, listSample):
-        listSample = list(map(lambda sample: sample / self.maxAmplutideInAllSample, listSample))
+        # превратили все в значения по модулю
+        listSample = list(map(lambda sample: fabs(sample / self.maxAmplutideInAllSample), listSample))
+        # listSample = list(map(lambda sample: sample / self.maxAmplutideInAllSample, listSample))
         return listSample
     
 
-    def calculateThreshold(self, listSample, coefficient = 1.0):            
-        self.threshold_1 = coefficient * (sum(listSample) / len(listSample))
+    def calculateThreshold(self, listSample, coefficient = 1.0):   
+            # return fabs(coefficient * (sum(listSample) / len(listSample)))
+        return fabs(coefficient * (sum(listSample) / len(listSample)))
+    
 
-
-    def chunks(self, lst, n):
-        """Yield successive n-sized chunks from lst."""
-        for i in range(0, len(lst), n):
-            chunk = lst[i:i + n]
-            yield chunk, max(chunk)
-
-
-    def checkMutenessUsingTreshold_1(self, listSample):
+    def splitWord(self, lst, n, threshold):
+        startWordIndex = 0
+        endWordIndex = 0
         splitWord = []
-        resultChunks = self.chunks(listSample, self.initSizeWindow)
+        self.sizeWindow = sys.maxsize
+
+        for i in range (0, len(lst), 1):
+            if (endSlice := i + n) == len(lst) + 2:
+                break
+            
+            if max(lst[i:endSlice], key = lambda i: fabs(i), default = 0) < threshold:
+                if (difference := endWordIndex - startWordIndex) >= n * 10:
+                    print(f'{n * 10}, {startWordIndex} : {endWordIndex}')
+                    splitWord.append({(startWordIndex * _SECOND_TO_MILLISECOND / self.sampleRate , endWordIndex* _SECOND_TO_MILLISECOND / self.sampleRate): lst[startWordIndex:endWordIndex]})
+                    if difference < self.sizeWindow:
+                        self.sizeWindow = difference
+                startWordIndex, endWordIndex = i, i
+            else:
+                endWordIndex = i
+        return splitWord
+
+
+
+    # def checkMutenessUsingTreshold_1(self, listSample):
+    #     splitWord = []
+    #     for word in self.splitWord(listSample, self.initSizeWindow, self.threshold_1):
+    #         splitWord.extend(word)
+    #     fig, ax = plt.subplots(figsize=(20,3))
+    #     ax.plot(splitWord)
+    #     # ax.axhline(y = self.threshold_1, color='red')
+    #     plt.show()
+        # return splitWord
+
 
 
 
     def searchWordsInAudioFragment(self, start: float, finish: float):
         fragment = self.audioData[start:finish].get_array_of_samples()
+        # fragment = nr.reduce_noise(y = fragment, sr=self.sampleRate, stationary=True)
+
         self.maxAmplutideInAllSample = fabs(max(fragment, key = lambda i: fabs(i), default = 0))
         fragment = self.normalization(fragment)
 
-        self.calculateThreshold(fragment, 0.55)
-        self.checkMutenessUsingTreshold_1(fragment)
+        # fig, ax = plt.subplots(figsize=(20,3))
+        # ax.plot(fragment)
+        # plt.show()
+
+        self.threshold_1 = self.calculateThreshold(fragment, 0.55)
+        self.threshold_2 = self.calculateThreshold(fragment)
+        self.splitWords = self.splitWord(fragment, self.sizeWindow, self.threshold_1)
+        # fragment = self.checkMutenessUsingTreshold_2(fragment)
 
 
         # coefficientMillSecondToSize = len(fragment) // (finish - start)
@@ -72,8 +111,20 @@ class SearchWord:
 
         # fig, ax = plt.subplots(figsize=(20,3))
         # ax.plot(fragment)
+        # ax.axhline(y = self.threshold_1, color='red')
         # plt.show()
         # print(librosa.get_duration(y=self.data, sr=self.sampleRate))
+
+
+    def getWavWord(self, start):
+        i = 0
+        for word in self.splitWords:
+
+            for time in word:
+
+                trimmed_audio = self.audioData[start + time[0]:start + time[1]]
+                trimmed_audio.export(f"/home/dasha/python_diplom/cut_res/{i}.wav", format="wav")
+                i += 1
 
 
 
@@ -92,3 +143,4 @@ if __name__=='__main__':
     search = SearchWord('/home/dasha/python_diplom/wav/five-in-sentence.wav')
 
     search.searchWordsInAudioFragment(945.0, 1830.0)
+    search.getWavWord(945.0)
